3ad1e7850924f006140d4c16c89352e67293a14f
Dan <danbeladev@gmail.com>
2019-09-08 19:15:10 +0300

    adding remote branch exception

---
+++ Logic/src/MagitExceptions/RemoteTrackingBranchException.java
@@ -0,0 +1,7 @@
+package MagitExceptions;
+
+import Lib.RemoteTrackingBranch;
+
+public class RemoteTrackingBranchException extends Exception {
+    public RemoteTrackingBranchException(String s){super(s);}
+}
